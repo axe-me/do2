da7e103458f2a2c4a756e78a8225de917196b161
ShaneXie <shane369@gmail.com>
2015-07-02 01:58:17 -0500

    multiple views not working

--- client/src/app/live/live.module.js
+++ client/src/app/live/live.module.js
@@ -3,21 +3,20 @@
 // available modules :
 // 'ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap
 angular.module('do2.live', ['ui.router'])
-.config(function ($stateProvider, $urlRouterProvider) {
-	$stateProvider
-	.state('home', {
-		url: '/',
-		templateUrl: 'app/live/live-index.html',
-		//controller: 'LiveCtrl',
-		views: {
-			'leftPanel': {
-				template: 'live.leftPanel'
-			},
-			'box': {
-				template: 'live.box'
-			}
-		}
-	});
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('live', {
+        url: '/',
+        views: {
+        	'': {templateUrl: 'app/live/live-index.html'},
+        	'leftPanel': {
+        		template: 'leftPanel'
+        	},
+        	'box': {
+        		template: 'box'
+        	}
+        }
+      });
 
-	$urlRouterProvider.otherwise('/');
-});
+    $urlRouterProvider.otherwise('/');
+  });
