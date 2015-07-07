7c4d168dff745db3bcfe85258cf79179487e2f97
ShaneXie <shane369@gmail.com>
2015-07-06 19:24:32 -0500

    done basic layout and some lint config

--- client/src/app/live/live.controller.js
+++ client/src/app/live/live.controller.js
@@ -2,6 +2,13 @@
 
 angular.module('do2.live').controller('LiveCtrl', LiveCtrl);
 
-function LiveCtrl($scope) {
+function LiveCtrl($scope, streamListFactory) {
+	angular.extend($scope, {
+	});
+
+	streamListFactory.getRawStreamListBySite('twitch')
+		.success(function (data) {
+			console.log(data);
+		});
 	
 }
