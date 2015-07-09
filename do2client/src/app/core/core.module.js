0465c0c26e1a2242d8d0441558c1142892f576f7
ShaneXie <shane369@gmail.com>
2015-07-09 01:01:44 -0500

    test color

--- do2client/src/app/core/core.module.js
+++ do2client/src/app/core/core.module.js
@@ -8,15 +8,15 @@ angular.module('do2.core', [])
 	.config(function($mdThemingProvider) {
 		$mdThemingProvider.theme('default')
 				.primaryPalette('red', {
-					'default': '400',
-					'hue-1': '100',
-					'hue-2': '600',
-					'hue-3': 'A100'})
+					'default': '900',
+					'hue-1': '500',
+					'hue-2': 'A400',
+					'hue-3': 'A700'})
 				.accentPalette('yellow', {
-					'default': '400',
-					'hue-1': '100',
-					'hue-2': '600',
-					'hue-3': 'A100'})
+					'default': 'A200',
+					'hue-1': '300',
+					'hue-2': '500',
+					'hue-3': '800'})
 				.warnPalette('lime', {
 					'default': '400',
 					'hue-1': '100',
