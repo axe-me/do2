549abef132f5120e74ee1c6fcb495d296fc3cf65
ShaneXie <shane369@gmail.com>
2015-07-11 17:40:49 -0500

    list ui

--- do2client/src/app/core/streamListFactory.js
+++ do2client/src/app/core/streamListFactory.js
@@ -3,12 +3,14 @@
 angular.module('do2.core')
 .factory('streamListFactory', function($http) {
 
+	var server_root = 'http://127.0.0.1:8000';
+
 	var apiUrl = {
-		douyu: 'http://127.0.0.1:8000/stream_api/0/?callback=JSON_CALLBACK',
-		zhanqi: 'http://127.0.0.1:8000/stream_api/1/?callback=JSON_CALLBACK',
-		huomao: 'http://127.0.0.1:8000/stream_api/2/?callback=JSON_CALLBACK',
-		huya: 'http://127.0.0.1:8000/stream_api/3/?callback=JSON_CALLBACK',
-		twitch: 'http://127.0.0.1:8000/stream_api/4/?callback=JSON_CALLBACK'
+		douyu: server_root + '/stream_api/0/?callback=JSON_CALLBACK',
+		zhanqi: server_root + '/stream_api/1/?callback=JSON_CALLBACK',
+		huomao: server_root + '/stream_api/2/?callback=JSON_CALLBACK',
+		huya: server_root + '/stream_api/3/?callback=JSON_CALLBACK',
+		twitch: server_root + '/stream_api/4/?callback=JSON_CALLBACK'
 	};
 
 	var streamListFactory = {
